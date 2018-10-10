553438c59ccfbc6d6c051752785b59b6723f8100
Samantha Young <samantha.young@students.olin.edu>
2018-10-10 11:28:39 -0400

    Adding testcases and test benches

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,7 +1,8 @@
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
-
+`timescale 1 ns / 1 ps
+`include "shiftregister.v"
 module testshiftregister();
 
     reg             clk;
@@ -20,9 +21,51 @@ module testshiftregister();
     		           .serialDataIn(serialDataIn),
     		           .parallelDataOut(parallelDataOut),
     		           .serialDataOut(serialDataOut));
+    // Generate clock (50MHz)
+    initial clk=0;
+    always #10 clk=!clk;    // 50MHz Clock
 
     initial begin
-    	// Your Test Code
+    $dumpfile("shifter.vcd");
+    $dumpvars(0, dut);
+
+
+    //Case 1: PIPO
+    //Parallel load is always asserted, 8 bits are loaded into place. After these 8 bits are loaded, parallel data in goes to zero, and we observe a full shift in the output.
+    parallelDataIn = 8'b11001100;
+    parallelLoad= 1;
+    #100;
+    $display("current register   %b", parallelDataOut);
+    parallelDataIn = 8'b0000000;
+    #100;
+    $display("current register   %b", parallelDataOut);
+    parallelDataIn = 8'b1010101;
+    #100;
+    $display("current register   %b", parallelDataOut);
+
+
+
+    //Case 2: SIS0
+    //Parallel load is never asserted, we observe a reading of serial data into the LSB
+    parallelLoad = 0;
+    serialDataIn = 1;
+    #100;
+    $display("current register LSB   %b", parallelDataOut); // not working as expected
+    serialDataIn = 0;
+    #100;
+    $display("current register LSB   %b", serialDataOut);
+    serialDataIn = 1;
+    #100;
+    $display("current register LSB   %b", serialDataOut);
+
+    //Case 3:
+    //Peripheral clock edge is non-periodic, so we observe non-periodic shifting. Parallel load is always asserted.
+
+    //Case 4:
+    //Parallel load is toggled.
+
+    $dumpflush;
+    $finish;
     end
 
 endmodule
--- shiftregister.v
+++ shiftregister.v
@@ -14,15 +14,17 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg[width-1:0]  parallelDataOut,    // Shift reg data contents
+output reg           serialDataOut       // Positive edge synchronized
 );
 
     reg [width:0]      shiftregistermem;
     always @(posedge clk) begin
         if( parallelLoad == 1)
           // When parallelLoad is asserted, the shift register will take the value of parallelDataIn.
-          parallelDataOut <= parallelDataIn;
+
+        parallelDataOut <= parallelDataIn;
+        //--
         else if (peripheralClkEdge == 1) begin
           // When the peripheral clock peripheralClkEdge has an edge, the shift register advances one position:
           // serialDataIn is loaded into the LSB (Least Significant Bit), and the rest of the bits shift up by one.
