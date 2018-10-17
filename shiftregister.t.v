39e2409469575f2c3fcc116832b7c047cb74714b
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-10-17 10:11:17 -0400

    shiftregister test bench

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -25,7 +25,7 @@ module testshiftregister();
     initial clk=0;
     initial peripheralClkEdge = 0;
     always #10 clk=!clk;    // 50MHz Clock
-/*
+
     always
       begin
       peripheralClkEdge <= 1'b0;
@@ -35,99 +35,99 @@ module testshiftregister();
       peripheralClkEdge <= 1'b0;
       #9;
       end
-*/
+
     initial begin
     $dumpfile("shifter.vcd");
     $dumpvars(0, dut);
 
-    //Hey Emma! I think the reason that serial stuff doesnt work is because we arent setting peripheral clock edge to anything. I think this clock edge comes from the
-    //input conditioner "rising" variable. But I am not entirely sure what input we need to condition? I think its a button press on the FPGA but for this test, just a  random
-    // on off signal ?  I think so. Lets chat
-
     //Case 1: PIPO
     //Parallel load is always asserted, 8 bits are loaded into place. After these 8 bits are loaded, parallel data in goes to zero, and we observe a full shift in the output.
-    $display("PIPO");
 
-    parallelLoad = 1;
-    parallelDataIn = 8'b11001100;
+    parallelLoad = 1; parallelDataIn = 8'b11001100; #50
+   if(parallelDataIn != parallelDataOut) begin
+     $display("error in test #1" );
+     $display("Parallel Out | Expected: %d Got: %d", parallelDataIn, parallelDataOut);
+   end
+   else begin
+     $display("Passed Test 1 : PIPO with %d", parallelDataIn);
+     end
 
-    #50;
-    $display("current register   %b", parallelDataOut);
-    parallelDataIn = 8'b0000000;
-    #50;
-    $display("current register   %b", parallelDataOut);
-    parallelDataIn = 8'b1010101;
-    #50;
-    $display("current register   %b", parallelDataOut);
+     parallelLoad = 1; parallelDataIn = 8'b0000000; #50
+    if(parallelDataIn != parallelDataOut) begin
+      $display("error in test #1" );
+      $display("Parallel Out | Expected: %d Got: %d", parallelDataIn, parallelDataOut);
+    end
+    else begin
+      $display("Passed Test 1 : PIPO with %d", parallelDataIn);
+      end
+
+
+      parallelLoad = 1; parallelDataIn = 8'b1010101; #50
+      if(parallelDataIn != parallelDataOut) begin
+       $display("error in test #1" );
+       $display("Parallel Out | Expected: %d Got: %d", parallelDataIn, parallelDataOut);
+      end
+      else begin
+       $display("Passed Test 1 : PIPO with %d", parallelDataIn);
+       end
+
 
 
 
     //Case 2: SIS0
     //Parallel load is never asserted, we observe a reading of serial data into the LSB
 
-    parallelLoad = 1;
-    parallelDataIn = 8'b1010101;
-    #50
-    // not working as expected // GTKWAVE does not register serial dataout as getting anything written to it. we should look at shift register.v
-    $display("Parallel Output Register   %b", parallelDataOut);
-    parallelLoad = 0;
-    serialDataIn = 0;
-    //parallelDataIn = 8'b0010100;
-    #20;
-    $display("current register LSB   %b", serialDataOut);
-    serialDataIn = 1;
-    //parallelDataIn = 8'b1000101;
-    #50;
-    $display("current register LSB   %b", serialDataOut);
+    parallelLoad = 1; parallelDataIn = 8'b00000000; #50
+    if(parallelDataIn != parallelDataOut) begin
+     $display("error in test #2" );
+     $display("ParallelOut | Expected: %d Got: %d", parallelDataIn, parallelDataOut);
+    end
+    else begin
+     $display("Passed Test 2 : Load zeros through parallelIn");
+     end
 
+    parallelLoad = 0; parallelDataIn = 8'b1010101; serialDataIn = 1; #50
+    if(serialDataOut != 0) begin
+     $display("error in test #2" );
+     $display("Serial Out | Expected: %d Got: %d", serialDataIn, serialDataOut);
+    end
+    else begin
+     $display("Passed Test 2 : Reads Correct Bit");
+     end
 
-    //case 3:
-    //Case 2: SIPO
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+     serialDataIn = 1; #50
+
+     if(serialDataOut != 1) begin
+      $display("error in test #2" );
+      $display("Serial Out | Expected: 1 Got: %d", serialDataOut);
+     end
+     else begin
+      $display("Passed Test 2 : SISO with %d", serialDataIn);
+      end
+
+    //Case 3: SIPO
     //Parallel load is never asserted, we observe a reading of serial data into the LSB
-    parallelLoad = 1;
-    parallelDataIn = 8'b00000000;
-    #50
-    $display("SIPO   %b", parallelDataOut);
-    parallelLoad = 0;
-    serialDataIn = 1;
-    peripheralClkEdge = 1;
-    $display("current register   %b", parallelDataOut);
-    #20
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 0;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
-    serialDataIn = 1;
-    $display("current register   %b", parallelDataOut);
-    #50
+    parallelLoad = 0; serialDataIn = 0; #100
 
-    $display("next case");
+    serialDataIn = 1; #60
+    serialDataIn = 0; #80
+    serialDataIn = 1; #25
 
-    #15;
-    // not working as expected // GTKWAVE does not register serial dataout as getting anything written to it. we should look at shift register.v
-    $display("current register   %b", parallelDataOut);
-    serialDataIn = 0;
-    //parallelDataIn = 8'b0010100;
-    #20;
-    $display("current register LSB   %b", serialDataOut);
-    serialDataIn = 1;
-    //parallelDataIn = 8'b1000101;
-    #20;
-    $display("current register LSB   %b", serialDataOut);
+    if(parallelDataOut != 225) begin
+     $display("error in test #3" );
+     $display("Parallel Out | Expected: 10010110 Got: %d", parallelDataOut);
+    end
+    else begin
+     $display("Passed Test 3 : SIPO with %d", parallelDataOut);
+     end
+     
     //Case 3:
     //Peripheral clock edge is non-periodic, so we observe non-periodic shifting. Parallel load is always asserted.
 
